d4eb966d527d81fbf8ecda0bd594f0eccb5d74ae
Shaheer Ahmed <shaheerahmed85@gmail.com>
2017-12-24 13:37:17 +0530

    Add files via upload

---
+++ facechatmiddleware/src/main/java/com/facechat/facechatmiddleware/controller/BlogController.java
@@ -0,0 +1,74 @@
+package com.facechat.facechatmiddleware.controller;
+
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.facechat.facechatbackend.dao.BlogDAO;
+import com.facechat.facechatbackend.model.Blog;
+
+@RestController
+public class BlogController {
+
+	@Autowired
+	BlogDAO blogDAO;
+	
+	@RequestMapping(value ="/insertBlog")
+	public ResponseEntity<String> insertBlog(@RequestBody Blog blog)
+	{		
+		if(blogDAO.addBlog(blog))
+		{
+			return new ResponseEntity<String>("Blog Added",HttpStatus.OK);
+		}
+		else
+		{
+			return new ResponseEntity<String>("Error in Response Entity",HttpStatus.SERVICE_UNAVAILABLE);
+		}
+	}
+	
+	@RequestMapping(value="/updateBlog")
+	public  ResponseEntity<String> updateBlog(@RequestBody Blog blog)
+	{
+		Blog tempBlog=blogDAO.getBlog(blog.getBlogId());
+		
+		
+		
+		tempBlog.setBlogName(blog.getBlogName());
+		tempBlog.setBlogContent(blog.getBlogContent());
+		if(blogDAO.updateBlog(tempBlog))
+		{
+			return new ResponseEntity<String>("Blog Update",HttpStatus.OK);
+			
+		}
+		else
+		{
+			return new ResponseEntity<String>("Error in Blog updation",HttpStatus.SERVICE_UNAVAILABLE);
+			
+		}
+	}
+	@RequestMapping(value="/getAllBlogs")
+	public ResponseEntity<ArrayList<Blog>> getAllBlogs()
+	{
+		ArrayList listBlogs = (ArrayList)blogDAO.getAllBlogs();
+		return new ResponseEntity<ArrayList<Blog>>(listBlogs,HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "deleteBlog")
+	public ResponseEntity<String> deleteBlog(@RequestBody Blog blog)
+	{
+		Blog tempblog=blogDAO.getBlog(blog.getBlogId());
+		if(blogDAO.deleteBlog(tempblog))
+		{
+			return new ResponseEntity<String>("Blog Deleted",HttpStatus.OK);
+		}
+		else{
+			return new ResponseEntity<String>("Error in blog deletion",HttpStatus.SERVICE_UNAVAILABLE);
+		}
+	}
+}
